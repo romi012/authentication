d98b1a52871e818f7c2b435c428ff593d6ea7eef
romi012 <r_iqbal01@yahoo.com>
2017-11-11 22:51:26 +0500

    First commit

---
+++ src/app/credentials.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by ROMI on 11/11/2017.
+ */
+export const firebaseConfig= {
+  apiKey: "AIzaSyB6W1swmsUvohkN1lfpiBE6INr_5aN697E",
+  authDomain: "eventmanager-f60ff.firebaseapp.com",
+  databaseURL: "https://eventmanager-f60ff.firebaseio.com",
+  projectId: "eventmanager-f60ff",
+  storageBucket: "eventmanager-f60ff.appspot.com",
+  messagingSenderId: "739155250286"
+};
